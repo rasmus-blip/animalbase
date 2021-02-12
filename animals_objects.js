367ede55d9ef8a3fe8aca6ffaf99108a0e874158
Rasmus Møller <rasm38h8@stud.kea.dk>
2021-02-12 08:53:52 +0100

    all data is cleaned and appends, except for description

--- animals_objects.js
+++ animals_objects.js
@@ -4,52 +4,78 @@ window.addEventListener("DOMContentLoaded", start);
 
 const allAnimals = [];
 
-function start( ) {
-    console.log("ready");
+function start() {
+  console.log("ready");
 
-    loadJSON();
+  loadJSON();
 }
-
 
 function loadJSON() {
-    fetch("animals.json")
-    .then( response => response.json() )
-    .then( jsonData => {
-        // when loaded, prepare objects
-        prepareObjects( jsonData );
+  fetch("animals.json")
+    .then((response) => response.json())
+    .then((jsonData) => {
+      // when loaded, prepare objects
+      prepareObjects(jsonData);
     });
+  console.log("loaded");
 }
 
-function prepareObjects( jsonData ) {
-    jsonData.forEach( jsonObject => {
-        // TODO: Create new object with cleaned data - and store that in the allAnimals array
-        
-        // TODO: MISSING CODE HERE !!!
-    });
+function prepareObjects(jsonData) {
+  jsonData.forEach((jsonObject) => {
+    // TODO: Create new object with cleaned data - and store that in the allAnimals array
 
-    displayList();
+    const firstSpace = jsonObject.fullname.indexOf(" ");
+    const lastSpace = jsonObject.fullname.lastIndexOf(" ");
+    // const middleSpace = jsonObject.fullname.indexOf(firstSpace, " ");
+    const nameAnimal = jsonObject.fullname.substring(0, firstSpace).trim();
+    const type = jsonObject.fullname.substring(lastSpace).trim();
+    // const description = jsonObject.fullname
+    //   .substring(middleSpace, lastSpace)
+    //   .trim();
+    const age = jsonObject.age;
+
+    const animal = {
+      name: nameAnimal,
+      //   desc: description, MANGLER!!
+      type: type,
+      age: age,
+    };
+    console.log(animal);
+
+    allAnimals.name = animal.name;
+    allAnimals.type = animal.type;
+    // allAnimals.desc = animal.description;
+    allAnimals.age = animal.age;
+
+    allAnimals.push(animal);
+    delete allAnimals.fullname;
+
+    // TODO: MISSING CODE HERE !!!
+  });
+
+  displayList();
 }
 
 function displayList() {
-    // clear the list
-    document.querySelector("#list tbody").innerHTML = "";
+  // clear the list
+  document.querySelector("#list tbody").innerHTML = "";
 
-    // build a new list
-    allAnimals.forEach( displayAnimal );
+  // build a new list
+  allAnimals.forEach(displayAnimal);
 }
 
-function displayAnimal( animal ) {
-    // create clone
-    const clone = document.querySelector("template#animal").content.cloneNode(true);
+function displayAnimal(animal) {
+  // create clone
+  const clone = document
+    .querySelector("template#animal")
+    .content.cloneNode(true);
 
-    // set clone data
-    clone.querySelector("[data-field=name]").textContent = animal.name;
-    clone.querySelector("[data-field=desc]").textContent = animal.desc;
-    clone.querySelector("[data-field=type]").textContent = animal.type;
-    clone.querySelector("[data-field=age]").textContent = animal.age;
+  // set clone data
+  clone.querySelector("[data-field=name]").textContent = animal.name;
+  // clone.querySelector("[data-field=desc]").textContent = animal.desc;
+  clone.querySelector("[data-field=type]").textContent = animal.type;
+  clone.querySelector("[data-field=age]").textContent = animal.age;
 
-    // append clone to list
-    document.querySelector("#list tbody").appendChild( clone );
+  // append clone to list
+  document.querySelector("#list tbody").appendChild(clone);
 }
-
-
